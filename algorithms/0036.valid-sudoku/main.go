1cf2160509eb5fdc1ca9de437815133105132950
Allenxuxu <120582243@qq.com>
2020-04-06 21:54:56 +0800

    add 0036

---
+++ algorithms/0036.valid-sudoku/main.go
@@ -0,0 +1,35 @@
+package algorithm
+
+func isValidSudoku(board [][]byte) bool {
+	var row [9][10]bool
+	var col [9][10]bool
+	var mb [9][10]bool
+
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] != '.' {
+				number := board[i][j] - '0'
+				if row[i][number] == true {
+					return false
+				} else {
+					row[i][number] = true
+				}
+
+				if col[j][number] == true {
+					return false
+				} else {
+					col[j][number] = true
+				}
+
+				index := (i/3)*3 + j/3
+				if mb[index][number] == true {
+					return false
+				} else {
+					mb[index][number] = true
+				}
+			}
+		}
+	}
+
+	return true
+}
