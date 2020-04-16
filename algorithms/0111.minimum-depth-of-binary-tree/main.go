e1998d5c61f08ed37cca4ead41664d22879f0ab7
Allenxuxu <120582243@qq.com>
2020-04-16 09:10:36 +0800

    add 0111

---
+++ algorithms/0111.minimum-depth-of-binary-tree/main.go
@@ -0,0 +1,74 @@
+package algorithm
+
+import (
+	"container/list"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	switch {
+	case root == nil:
+		return 0
+	case root.Left == nil:
+		return 1 + minDepth(root.Right)
+	case root.Right == nil:
+		return 1 + minDepth(root.Left)
+	default:
+		return 1 + min(minDepth(root.Left), minDepth(root.Right))
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minDepth1(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	type Entry struct {
+		*TreeNode
+		Level int
+	}
+
+	queue := list.New()
+	queue.PushBack(&Entry{
+		TreeNode: root,
+		Level:    1,
+	})
+	var ret int
+	for queue.Len() != 0 {
+		item := queue.Front()
+		node := item.Value.(*Entry)
+		queue.Remove(item)
+
+		ret = node.Level
+
+		if node.Left != nil {
+			queue.PushBack(&Entry{
+				TreeNode: node.Left,
+				Level:    node.Level + 1,
+			})
+		}
+		if node.Right != nil {
+			queue.PushBack(&Entry{
+				TreeNode: node.Right,
+				Level:    node.Level + 1,
+			})
+		}
+		if node.Left == nil && node.Right == nil {
+			break
+		}
+	}
+
+	return ret
+}
---
+++ algorithms/0111.minimum-depth-of-binary-tree/main_test.go
@@ -0,0 +1,71 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	pre, in []int
+	ans     int
+}{
+
+	{
+		[]int{1, 2, 3, 4, 5},
+		[]int{5, 4, 3, 2, 1},
+		5,
+	},
+
+	{
+		[]int{1, 2},
+		[]int{2, 1},
+		2,
+	},
+
+	{
+		[]int{1},
+		[]int{1},
+		1,
+	},
+
+	{
+		[]int{},
+		[]int{},
+		0,
+	},
+
+	{
+		[]int{2, 1, 5, 4, 3, 6, 7},
+		[]int{1, 2, 3, 4, 5, 6, 7},
+		2,
+	},
+
+	{
+		[]int{4, 2, 1, 3, 6, 5, 7},
+		[]int{1, 2, 3, 4, 5, 6, 7},
+		3,
+	},
+
+	// 可以多个 testcase
+}
+
+func Test_minDepth(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, minDepth(PreIn2Tree(tc.pre, tc.in)), "输入:%v", tc)
+	}
+}
+
+func Test_minDepth1(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, minDepth1(PreIn2Tree(tc.pre, tc.in)), "输入:%v", tc)
+	}
+}
