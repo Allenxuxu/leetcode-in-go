5f36997b0ff49ad306703ed7cc1e4cc902fd2b21
Allenxuxu <120582243@qq.com>
2020-04-25 11:17:28 +0800

    add 0094

---
+++ algorithms/0094.binary-tree-inorder-traversal/main.go
@@ -0,0 +1,52 @@
+package algorithm
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var ret []int
+	helper(root, &ret)
+	return ret
+}
+
+func helper(root *TreeNode, data *[]int) {
+	if root == nil {
+		return
+	}
+	helper(root.Left, data)
+	*data = append(*data, root.Val)
+	helper(root.Right, data)
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var ret []int
+	helperPreOrder(root, &ret)
+	return ret
+}
+
+func helperPreOrder(root *TreeNode, data *[]int) {
+	if root == nil {
+		return
+	}
+	*data = append(*data, root.Val)
+	helperPreOrder(root.Left, data)
+	helperPreOrder(root.Right, data)
+}
+
+func postOrderTraversal(root *TreeNode) []int {
+	var ret []int
+	helperPostOrder(root, &ret)
+	return ret
+}
+
+func helperPostOrder(root *TreeNode, data *[]int) {
+	if root == nil {
+		return
+	}
+	*data = append(*data, root.Val)
+	helperPostOrder(root.Left, data)
+	helperPostOrder(root.Right, data)
+}
