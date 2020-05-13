c648264dc438603f28a0396ec155c6630aac702d
Allenxuxu <120582243@qq.com>
2020-05-13 20:05:20 +0800

    优化 0076

--- algorithms/0076.minimum-window-substring/main.go
+++ algorithms/0076.minimum-window-substring/main.go
@@ -1,35 +1,47 @@
 package algorithm
 
-import (
-	"strings"
-)
-
 func minWindow(s string, t string) string {
 	var ret string
 	need := make(map[uint8]int, len(t))
+	have := make(map[uint8]int, len(t))
 	for i := range t {
 		need[t[i]]++
 	}
 
 	var tmp string
+	var flag int
 	for left, right := 0, 1; right <= len(s); right++ {
 		tmp = s[left:right]
+
+		if need[s[right-1]] != 0 {
+			have[s[right-1]]++
+			if have[s[right-1]] == need[s[right-1]] {
+				flag++
+			}
+		}
 
 		if len(tmp) < len(t) {
 			continue
 		}
-
-		if valid(tmp, need) {
+		if flag == len(need) {
 			if len(ret) == 0 || len(tmp) < len(ret) {
 				ret = tmp
 			}
 
 			for {
+				if need[s[left]] != 0 {
+					if need[s[left]] == have[s[left]] {
+						flag--
+					}
+					have[s[left]]--
+				}
+
 				left++
 				tmp = s[left:right]
-				if !valid(tmp, need) {
+				if flag != len(need) {
 					break
 				}
+
 				if len(tmp) < len(ret) {
 					ret = tmp
 				}
@@ -38,14 +50,4 @@ func minWindow(s string, t string) string {
 	}
 
 	return ret
-}
-
-func valid(s string, m map[uint8]int) bool {
-	for k, v := range m {
-		if strings.Count(s, string(k)) < v {
-			return false
-		}
-	}
-
-	return true
 }
