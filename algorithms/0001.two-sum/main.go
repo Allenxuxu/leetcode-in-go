e4c2fc4cdbca5bcc0eac6488f1af3148f912804d
Allenxuxu <120582243@qq.com>
2020-05-14 14:37:25 +0800

    增加 注释

--- algorithms/0001.two-sum/main.go
+++ algorithms/0001.two-sum/main.go
@@ -4,6 +4,7 @@ package algorithm
 // 空间复杂度 O(n)
 // 将数组中所有元素插入 map 中， key 为数组元素的值， value 为数组下标
 // 遍历数组，用 target 减去 v， 如果其差值存在于 map 中则记录两者 index 退出循环
+// 注意：如果存在重复元素，构建 map 时，数组下表较大的会覆盖较小的。而第二次遍历时，会先取出较小下表的元素，因此可以通过case。
 func twoSum(nums []int, target int) []int {
 	var ret []int
 	m := make(map[int]int, len(nums))
