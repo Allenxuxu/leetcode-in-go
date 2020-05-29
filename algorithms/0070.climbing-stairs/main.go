7532ea0b39726e6449529370397cd17e6df8a8aa
Allenxuxu <120582243@qq.com>
2020-05-29 20:00:19 +0800

    u

--- algorithms/0070.climbing-stairs/main.go
+++ algorithms/0070.climbing-stairs/main.go
@@ -1,6 +1,24 @@
 package algorithm
 
+// 要爬到第 n 阶，有两种方式：
+// 从第 n - 1 阶上去(一次跨 1 阶)
+// 从第 n - 2 阶上去(一次跨 2 阶)
+// 所以 climbStairs(n) = climbStairs(n-1) + climbStairs(n-2)
 func climbStairs(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+
+	return climbStairs(n-1) + climbStairs(n-2)
+}
+
+func climbStairs1(n int) int {
 	if n == 0 {
 		return 0
 	}
@@ -16,4 +34,25 @@ func climbStairs(n int) int {
 		f1, f2 = f2, f1+f2
 	}
 	return f2
+}
+
+func climbStairs2(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+
+	rec := make([]int, n+1)
+	rec[1], rec[2] = 1, 2
+
+	for i := 3; i <= n; i++ {
+		rec[i] = rec[i-1] + rec[i-2]
+	}
+
+	return rec[n]
 }
--- algorithms/0070.climbing-stairs/main_test.go
+++ algorithms/0070.climbing-stairs/main_test.go
@@ -22,54 +22,73 @@ type ans struct {
 	one int
 }
 
+var qs = []question{
+
+	question{
+		para{
+			0,
+		},
+		ans{
+			0,
+		},
+	},
+
+	question{
+		para{
+			10,
+		},
+		ans{
+			89,
+		},
+	},
+
+	question{
+		para{
+			44,
+		},
+		ans{
+			1134903170,
+		},
+	},
+
+	question{
+		para{
+			1,
+		},
+		ans{
+			1,
+		},
+	},
+
+	// 如需多个测试，可以复制上方元素。
+}
+
 func Test_climbStairs(t *testing.T) {
 	ast := assert.New(t)
-
-	qs := []question{
-
-		question{
-			para{
-				0,
-			},
-			ans{
-				0,
-			},
-		},
-
-		question{
-			para{
-				10,
-			},
-			ans{
-				89,
-			},
-		},
-
-		question{
-			para{
-				44,
-			},
-			ans{
-				1134903170,
-			},
-		},
-
-		question{
-			para{
-				1,
-			},
-			ans{
-				1,
-			},
-		},
-
-		// 如需多个测试，可以复制上方元素。
-	}
 
 	for _, q := range qs {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
-
 		ast.Equal(a.one, climbStairs(p.n), "输入:%v", p)
+	}
+}
+
+func Test_climbStairs1(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+		ast.Equal(a.one, climbStairs1(p.n), "输入:%v", p)
+	}
+}
+
+func Test_climbStairs2(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+		ast.Equal(a.one, climbStairs2(p.n), "输入:%v", p)
 	}
 }
