c50cef1e5779aa2f2a45fe996e976b0775727dec
Allenxuxu <120582243@qq.com>
2020-07-18 18:02:37 +0800

    fix

--- algorithms/0160.intersection-of-two-linked-lists/main_test.go
+++ algorithms/0160.intersection-of-two-linked-lists/main_test.go
@@ -62,27 +62,6 @@ func Test_getIntersectionNode(t *testing.T) {
 	}
 }
 
-// Ints2ListWithCycle returns a list whose tail point to pos-indexed node
-// head's index is 0
-// if pos = -1, no cycle
-func Ints2ListWithCycle(nums []int, pos int) *ListNode {
-	head := Ints2List(nums)
-	if pos == -1 {
-		return head
-	}
-	c := head
-	for pos > 0 {
-		c = c.Next
-		pos--
-	}
-	tail := c
-	for tail.Next != nil {
-		tail = tail.Next
-	}
-	tail.Next = c
-	return head
-}
-
 // Ints2List convert []int to List
 func Ints2List(nums []int) *ListNode {
 	l := &ListNode{}
